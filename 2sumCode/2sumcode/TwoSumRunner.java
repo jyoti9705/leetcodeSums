ea41d78734e29b7c1053b8a4e9fd51de49d333a6
JyotiSingh <jyotimrjs9705@gmail.com>
2021-09-19 22:11:06 +0530

    Solution for returning indices when 2 nums in array add to target

---
+++ 2sumCode/2sumcode/TwoSumRunner.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class TwoSumRunner {
+
+    private static int[] getSumEqualsTargetIndices(int[] nums , int target){
+        for (int i = 0 ; i < nums.length ; i++){
+            for (int j = i+ i ; j < nums.length ; j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main (String [] args){
+        int [] nums = new int[]{2 , 7, 11, 15};
+        int target = 9;
+        int[] result = getSumEqualsTargetIndices(nums,target);
+        Arrays.stream(result).forEach(a ->
+            System.out.println("Indices that add upto target are: "+ a)
+        );
+    }
+}
