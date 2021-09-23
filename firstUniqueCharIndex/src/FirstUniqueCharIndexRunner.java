8dee57eb2f0b0f3c8b7588725f29b6825c1182c7
JyotiSingh <jyotimrjs9705@gmail.com>
2021-09-23 15:46:12 +0530

    Find Unique Index of first distinct character in String

---
+++ firstUniqueCharIndex/src/FirstUniqueCharIndexRunner.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstUniqueCharIndexRunner {
+    private static int uniqueCharIndex(String s) {
+        Map<Character, Integer> mapOfCharAndCount = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            mapOfCharAndCount.put(c, mapOfCharAndCount.getOrDefault(c, 0) + 1);
+        }
+        for (char c : s.toCharArray()) {
+            if (1 == mapOfCharAndCount.get(c)) {
+                return s.indexOf(String.valueOf(c));
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        uniqueCharIndex("loveleetcode");
+    }
+}
